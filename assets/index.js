9f7ca880c631dde08b7cdda3a04b411811d43835
SamuelZhaoY <samuel.zhao.yue@live.com>
2017-06-18 17:16:29 +0800

    update explore implementation 35% progress

--- assets/index.js
+++ assets/index.js
@@ -26,7 +26,8 @@ WDClient.prototype.init = function() {
     });
 
     // Start crawling
-    new NSCrawler(crawlerConfig, sessionId).initialize().crawl();
+    var crawler = new NSCrawler(crawlerConfig, sessionId).initialize();
+    setTimeout(crawler.crawl.bind(crawler), crawlerConfig.launchTimeout * 1000)
   });
 };
 
@@ -44,7 +45,10 @@ WDClient.prototype.send = function(url, method, body, callback) {
     })
       .then(parseJSON)
       .then(function(data) {
-        callback(data);
+        if (callback) {
+          callback(data)
+        }
+        return data;
       });
   } else {
     return fetch(`${this.server}${url}`, {
@@ -55,7 +59,10 @@ WDClient.prototype.send = function(url, method, body, callback) {
     })
       .then(parseJSON)
       .then(function(data) {
-        callback(data);
+        if (callback) {
+          callback(data);
+        }
+        return data;
       });
   }
 };
@@ -63,7 +70,6 @@ WDClient.prototype.send = function(url, method, body, callback) {
 window.wdclient = new WDClient({
   server: 'http://localhost:3456'
 });
-
 
 /** ----------------------------------------  AppCrawler Implementation: 1. Modeling  ----------------------------------------- **/
 
@@ -82,7 +88,7 @@ var NSTargetSearchType = {
 };
 
 function NSTargetElement() {
-  this.searchMethod = NSTargetSearchType.XPath;   // Searchable via XPath, Value, Identifier
+  this.searchMethod = NSTargetSearchType.Value;   // Searchable via XPath, Value, Identifier
   this.searchValue = null;                        // Search value, value of XPath, Value & Identifier
   this.actionType  = NSTargetActionType.CLICK;
   this.actionPerformTimes = 1;
@@ -95,7 +101,7 @@ function NSCrawlerConfig() {
   this.testingDepth = 16;
   this.takeScreenShot = true;
   this.autoCancelAlert = true;
-  this.newCommandTimeout = 1;
+  this.newCommandTimeout = 3;
   this.launchTimeout = 6;
   this.maxActionPerPage = 20;
   this.navigationBackKeyword =[];
@@ -114,19 +120,27 @@ NSCrawlerConfig.prototype.debugDesriptoin =  function () {
 
 /** Crawling Node: each of the tree node represents a unique user page  */
 function NSAppCrawlingTreeNode() {
-  this.pageId = "";     // Unique page id for each of the page
   this.path = "";       // Unique path which leads to current page
   this.parent = null;   // Parent ui element
   this.actions = {};    // Units in {value : NSAppCrawlingTreeNodeAction}
-  this.childen = [];    // Units in [NSAppCrawlingTreeNode]
+  this.digest = null;
 }
 
 NSAppCrawlingTreeNode.prototype.isFinishedBrowseing = function () {
-
+  return false;
 }
-  
-NSAppCrawlingTreeNode.prototype.digest = function () {
 
+NSAppCrawlingTreeNode.prototype.checkDigest = function () {
+  if (this.digest == null) {
+    return window.wdclient.send(`/wd/hub/session/${sessionId}/title`,`get`,null,null)
+      .then((title)  => {
+        this.digest = title
+      });
+  } else {
+    return new Promise((resolve) => {
+      resolve(this.digest);
+    });
+  }
 }
 
 function NSAppCrawlingTreeNodeAction() {
@@ -139,22 +153,57 @@ NSAppCrawlingTreeNodeAction.prototype.desription = function() {
 
 }
 
-/** Crawling Buffer **/
-function NSAppCrawlingBuffer() {
-  this.buffer = [];       // Node tree, which contains a set of NSAppCrawlingTreeNode
-}
-
 /** ----------------------------------------  AppCrawler Implementation: 2. Crawler Logic ----------------------------------------- **/
 
 function NSCrawler(config, sessionId) {
   this.config         = config;                     // Config in format of NSCrawlerConfig
   this.sessionId      = sessionId;                  // Session Id
-  this.crawlingBuffer = new NSAppCrawlingBuffer();  // The set of notes
+  this.crawlingBuffer = [];                         // The set of notes
   this.currentNode    = null;                       // Current node which is in crawling
 }
 
 NSCrawler.prototype.initialize = function () {
   return this;
+}
+
+NSCrawler.prototype.explore = function (source) {
+  console.log("-------> Checkout Console <--------");
+  console.log(source);
+  let node = new NSAppCrawlingTreeNode();
+  node.checkDigest().then(() => {
+    // 1. Check if there is an existing node
+    for (let index in this.crawlingBuffer) {
+      if(this.crawlingBuffer[index] && this.crawlingBuffer[index].digest == node.digest) {
+        this.currentNode = this.crawlingBuffer[index];
+        setTimeout(this.prepareForNextScanning.bind(this), this.config.newCommandTimeout * 1000);
+        return;
+      }
+    }
+
+    // 2. Initialize an new node
+    node.parent = this.currentNode;
+    this.currentNode = node;
+
+    let match = recursiveFilter(JSON.parse(source.value), this.config.targetElements);
+    if (match.length) {
+       // Produce actions
+    } else {
+      //TODO: think about elements which no need to be triggered
+
+    }
+
+    setTimeout(this.prepareForNextScanning.bind(this), this.config.newCommandTimeout * 1000);
+  });
+}
+
+NSCrawler.prototype.prepareForNextScanning = function () {
+  if (this.currentNode.isFinishedBrowseing()) {
+    window.wdclient
+      .send(`/wd/hub/back`,`post`,null,null)
+      .then(this.crawl())
+  } else {
+    this.crawl()
+  }
 }
 
 NSCrawler.prototype.crawl = function () {
@@ -163,6 +212,12 @@ NSCrawler.prototype.crawl = function () {
   console.log("----> Config Info: <----");
   console.log(this.config.debugDesriptoin());
   console.log("----> Crawling <----");
+
+  window.wdclient
+    .send(`/wd/hub/session/${sessionId}/source`,`get`,null,null)
+    .then((data)  => {
+       this.explore(data)
+    })
 }
 
 /** ----------------------------------------  AppCrawler Implementation: 3. Configuration & Run ----------------------------------------- **/
@@ -173,15 +228,15 @@ var loginPassword = new NSTargetElement();
 var loginButton   = new NSTargetElement();
 
 loginAccount.actionType     = NSTargetActionType.INPUT;
-loginAccount.searchValue    = "";
+loginAccount.searchValue    = "please input username";
 loginAccount.actionValue    = "中文+Test+12345678";
 
 loginPassword.actionType    = NSTargetActionType.INPUT;
-loginPassword.searchValue   = "";
+loginPassword.searchValue   = "please input password";
 loginPassword.actionValue   = "111111";
 
 loginButton.actionType      = NSTargetActionType.CLICK;
-loginButton.searchValue     = "";
+loginButton.searchValue     = "Login";
 
 var crawlerConfig = new NSCrawlerConfig();
 crawlerConfig.targetElements[loginAccount.searchValue] = loginAccount;
@@ -191,4 +246,53 @@ crawlerConfig.targetElements[loginButton.searchValue] = loginButton;
 crawlerConfig.navigationBackKeyword.push("返回")
 crawlerConfig.navigationBackKeyword.push("取消")
 
+/** -------------------------------------------           Utils                  ------------------------------------------------------- **/
 
+// If match is null or empty, put all elements which belongs to button, label,
+function recursiveFilter(source, matches) {
+  var sourceArray = [];
+
+  for (var key in source) {
+    if (source.hasOwnProperty(key)) {
+      if (typeof  source[key] == "object" && source[key] !== null) {
+        let result = recursiveFilter(source[key], matches);
+        sourceArray = sourceArray.concat(result);
+      } else if (source[key] != null) {
+        if (matches && matches.length) {
+          // Explicit mode
+          for (let match in matches) {
+            if ((source.value && source.value == match) ||
+              (source.name && source.name == match)     ||
+              (source.label && source.label == match)) {
+              return [source]
+            }
+          }
+        } else {
+          if (source.type) {
+            switch (source.type) {
+              case 'StaticText':
+              case 'Button':
+              case 'Cell':
+                break;
+              case 'PageIndicator':
+                break;
+              case 'TextField':
+              case 'SecureTextField':
+                break;
+              case 'Other':
+                break;
+              case 'Alert':
+                break;
+              default:
+            }
+          }
+        }
+      }
+    }
+  }
+  return sourceArray;
+}
+
+function produceNodeActions(rawElements) {
+
+}
