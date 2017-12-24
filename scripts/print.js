7a4fc85d2b4cd78f7d88cab56cef4afab3a6118e
xudafeng <xudafeng@126.com>
2017-12-24 18:02:33 +0800

    add homepage

--- scripts/print.js
+++ scripts/print.js
@@ -2,17 +2,17 @@
 
 'use strict';
 
+const EOL = require('os').EOL;
+const logo = require('macaca-logo');
+
 const _ = require('../lib/common/helper');
 
 const {
   chalk
 } = _;
 
-const EOL = require('os').EOL;
-const logo = require('macaca-logo');
-
 const pkg = require('../package');
 
 console.log('');
 logo.print();
-console.log(`${chalk.white('Macaca was successfully installed! Please visit: ')}${chalk.cyan(pkg.homepage)}${EOL}`);
+console.log(`${chalk.white('Macaca NoSmoke was successfully installed! Please visit: ')}${chalk.cyan(pkg.homepage)}${EOL}`);
