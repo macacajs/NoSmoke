bce0349e6d02e1567d4dc0934c711274f6ffab26
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-01-15 11:29:00 +0800

    refine model

--- public/hooks.js
+++ public/hooks.js
@@ -38,7 +38,7 @@ Hooks.prototype.performAction = function(action, crawler) {
  * Method to intercept the crawling process after an specific action has been performed
  * @Params: action the action which belongs to current active node, and has just been performed
  * @Params: crawler the crawler instance which contains the context information as well as crawler config
- * @Returns: a Promise to indicate the action has been handled and other wise the default logic will bypass it
+ * @Returns: a Promise to indicate the action has been handled and otherwise the default logic will bypass it
  * */
 Hooks.prototype.afterActionPerformed = function(action, crawler) {
   return null;
