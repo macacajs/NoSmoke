191c4b7a7d18fed18799cd773199fa5bc7acaaa3
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-11-04 15:18:17 +0800

    preprocess image, improve ocr accuracy

--- scripts/ocr.js
+++ scripts/ocr.js
@@ -1,17 +1,27 @@
-var Tesseract = require('tesseract.js')
-var filename = '/Users/Gangdooz/private/Web/NoSmoke/reports/2018-11-3-16-39-43/9bbee6c1-841a-4a76-a211-4d0291af1b7d.png'
+const Tesseract = require('tesseract.js')
+const filename = 'image.png'
+const Jimp = require('jimp');
 const path = require("path");
 
+// tesseract img ocr
 let tesseract = Tesseract.create({
   workerPath: path.join(__dirname, '../lib/tesseract/src/node/worker.js'),
   langPath: path.join(__dirname, '../lib/tesseract/langs/'),
   corePath: path.join(__dirname, '../lib/tesseract/src/index.js')
 });
 
-tesseract.recognize(filename)
-  .progress(function  (p) { console.log('progress', p)  })
-  .catch(err => console.error(err))
-  .then(function (result) {
-    console.log(result.text)
-    process.exit(0)
-  });
+// transfer img to black and white for optimization
+Jimp.read("test.png", (err, image) => {
+  image
+    .contrast(-0.4)
+    .greyscale()
+    .write(filename);
+
+  tesseract.recognize(filename)
+    .progress(function  (p) { console.log('progress', p)  })
+    .catch(err => console.error(err))
+    .then(function (result) {
+      console.log(result.text)
+      process.exit(0)
+    });
+});
