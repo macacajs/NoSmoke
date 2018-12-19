8231de0d0cea183720c4272fb815966a52b13976
SamuelZhaoY <samuel.zhao.yue@live.com>
2018-12-19 14:17:12 +0800

    update dp px ratio

--- test/nosmoke.test.js
+++ test/nosmoke.test.js
@@ -38,3 +38,42 @@ describe('#Check Regex', function() {
     assert.ok(crawler.checkContentMatch(regexMock, 'please\\s+input\\s+username', true), 'then appears');
   });
 });
+
+const utils = require('../lib/crawler/ocr-impl/ocr-utils');
+describe('#Check device relative device density', function () {
+  it('check iphone 4', function () {
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 4'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone4'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 4s'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone4s'), 2);
+  });
+
+  it('check iphone 5', function () {
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 5'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone5'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 5s'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 5 s'), 2);
+  });
+
+  it('check iphone 6 plus', function () {
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 6 +'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 6s +'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 6s plus'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 6 plus'), 3);
+  });
+
+  it('check iphone 7 plus', function () {
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 7 +'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 7s +'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 7s plus'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone 8 plus'), 3);
+  });
+
+  it('check iphone x', function () {
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone x'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone x s'), 3);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone x r'), 2);
+    assert.equal(utils.convert_iOS_DIPToPXRatio('iphone xc max'), 3);
+  });
+
+});
