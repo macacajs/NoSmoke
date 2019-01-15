aa31d11a1e298b05b0b01ffb99fd8f10278253cf
SamuelZhaoY <samuel.zhao.yue@live.com>
2019-01-15 15:35:20 +0800

    add 2 optional hooks

--- demo/hooks.js
+++ demo/hooks.js
@@ -3,12 +3,27 @@
 function Hooks(config, sessionId) {}
 
 /**
+ * Method to intercept before the crawling process starts
+ * @Params: crawler, the crawler instance which contains the context information as well as crawler config
+ * */
+Hooks.prototype.beforeCrawlingStart = async function(crawler) {
+  return null;
+};
+
+/**
  * Method to intercept the crawling process after an specific action has been performed
  * @Params: action the action which belongs to current active node, and has just been performed
  * @Params: crawler the crawler instance which contains the context information as well as crawler config
- * @Returns: a Promise to indicate the action has been handled and otherwise the default logic will bypass it
  * */
 Hooks.prototype.onActionPerformed = async function(action, crawler) {
+  return null;
+};
+
+/**
+ * Method to intercept after the crawling process finish, this is called exactly before the crawler process terminates
+ * @Params: crawler, the crawler instance which contains the context information as well as crawler config
+ * */
+Hooks.prototype.afterCrawlingFinish = async function(crawler) {
   return null;
 };
 
