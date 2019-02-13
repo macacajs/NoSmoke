5ebd4c5f812329179e27ab69874fe25e873e618c
SamuelZhaoY <samuel.zhao.yue@live.com>
2019-02-13 17:06:36 +0800

    refine preinstall script

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+Linux*)     machine=Linux;;
+Darwin*)    machine=Mac;;
+CYGWIN*)    machine=Cygwin;;
+MINGW*)     machine=MinGw;;
+*)          machine="UNKNOWN:${unameOut}"
+esac
+
+# Check os versions and install essential dependencies
+echo '--------------------------'
+echo 'update native libraries'
+echo '--------------------------'
+
+if [ $machine == "Mac" ]; then
+# Do something under Mac OS X platform
+echo 'install libs for Mac OS'
+brew ls --versions tesseract || brew install tesseract --with-all-languages
+brew ls --versions libimobiledevice || brew install libimobiledevice
+elif [ $machine == "Linux" ]; then
+# Do something under GNU/Linux platform
+echo 'install libs for Linux'
+exit 0
+elif [ $machine == "MINGW" ]; then
+# Do something under Windows NT platform
+echo 'install libs for windows'
+exit 0
+else
+exit 0
+fi
+
 # Preinstall macaca dependencies
 echo '--------------------------'
 echo 'update node dependencies:'
@@ -17,29 +48,3 @@ npm list -g macaca-android || npm install macaca-android -g
 echo 'updating macaca...'
 npm list -g macaca-cli || npm install macaca-cli -g
 
-# Check os versions and install essential dependencies
-echo '--------------------------'
-echo 'update native libraries'
-echo '--------------------------'
-
-unameOut="$(uname -s)"
-case "${unameOut}" in
-Linux*)     machine=Linux;;
-Darwin*)    machine=Mac;;
-CYGWIN*)    machine=Cygwin;;
-MINGW*)     machine=MinGw;;
-*)          machine="UNKNOWN:${unameOut}"
-esac
-
-if [ $machine == "Mac" ]; then
-    # Do something under Mac OS X platform
-    echo 'install libs for Mac OS'
-    brew ls --versions tesseract || brew install tesseract --with-all-languages
-    brew ls --versions libimobiledevice || brew install libimobiledevice
-elif [ $machine == "Linux" ]; then
-    # Do something under GNU/Linux platform
-    echo 'install libs for Linux'
-elif [ $machine == "MINGW" ]; then
-    # Do something under Windows NT platform
-    echo 'install libs for windows'
-fi
