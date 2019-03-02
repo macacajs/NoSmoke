94491fef3ed7394a4c8ad348c0dd5719766ab47e
SamuelZhaoY <samuel.zhao.yue@live.com>
2019-03-02 21:07:59 +0800

    refine preinstall scripts

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -8,14 +8,14 @@ if (os.platform().includes('darwin')) {
   console.log('install libs for Mac OS');
   childProcess.execSync('brew ls --versions tesseract || brew install tesseract --with-all-languages');
   childProcess.execSync('brew ls --versions libimobiledevice || brew install libimobiledevice');
+
+  console.log('updating ios-deploy...');
+  childProcess.execSync('npm list -g ios-deploy || npm install ios-deploy -g');
 } else {
   console.log(`platform type: ${os.platform()}, no preinstall actions configured, please install tesseract manually, please go https://github.com/tesseract-ocr/tesseract/wiki#windows for details`);
 }
 
 // preinstall macaca dependencies
-console.log('updating ios-deploy...');
-childProcess.execSync('npm list -g ios-deploy || npm install ios-deploy -g');
-
 console.log('updating macaca-ios...');
 childProcess.execSync('npm list -g macaca-ios || npm install macaca-ios -g');
 
