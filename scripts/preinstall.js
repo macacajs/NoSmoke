01284cabbe51ff2f088b103ced403e1604b3e92a
SamuelZhaoY <samuel.zhao.yue@live.com>
2019-03-02 21:40:06 +0800

    update

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -9,18 +9,20 @@ if (os.platform().includes('darwin')) {
   childProcess.execSync('brew ls --versions tesseract || brew install tesseract --with-all-languages');
   childProcess.execSync('brew ls --versions libimobiledevice || brew install libimobiledevice');
 
+  // preinstall macaca dependencies
   console.log('updating ios-deploy...');
   childProcess.execSync('npm list -g ios-deploy || npm install ios-deploy -g');
+
+  console.log('updating macaca-ios...');
+  childProcess.execSync('npm list -g macaca-ios || npm install macaca-ios -g');
+
+  console.log('updating macaca-android...');
+  childProcess.execSync('npm list -g macaca-android || npm install macaca-android -g');
+
+  console.log('updating macaca...');
+  childProcess.execSync('npm list -g macaca-cli || npm install macaca-cli -g');
+
 } else {
   console.log(`platform type: ${os.platform()}, no preinstall actions configured, please install tesseract manually, please go https://github.com/tesseract-ocr/tesseract/wiki#windows for details`);
 }
 
-// preinstall macaca dependencies
-console.log('updating macaca-ios...');
-childProcess.execSync('npm list -g macaca-ios || npm install macaca-ios -g');
-
-console.log('updating macaca-android...');
-childProcess.execSync('npm list -g macaca-android || npm install macaca-android -g');
-
-console.log('updating macaca...');
-childProcess.execSync('npm list -g macaca-cli || npm install macaca-cli -g');
